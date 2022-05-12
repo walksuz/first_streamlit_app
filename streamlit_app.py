6cdbdc48e7ccefcd5dcb5a0e27cdeb2ca13dcac7
walksuz <71907969+walksuz@users.noreply.github.com>
2022-05-12 12:10:02 -0500

    Update streamlit_app.py
    correct type

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 import pandas
-my-fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
