25fb7b1bc6b468ef4aca3f2dc74b73339d1dea17
walksuz <71907969+walksuz@users.noreply.github.com>
2022-05-12 12:09:12 -0500

    Update streamlit_app.py
    import pandas and read csv file
    put it in a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,7 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+import pandas
+my-fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
