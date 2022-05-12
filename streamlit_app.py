1d0c96a3a6930fb9d614eea5804dc075efebd7b4
walksuz <71907969+walksuz@users.noreply.github.com>
2022-05-12 12:14:39 -0500

    Update streamlit_app.py
    added fruit emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,8 @@ streamlit.text('🥗 Kale, spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+
+streamlit.header('🍌🍓🍒 Build Your Own Fruit Smoothie 🥝🍇🍊')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
