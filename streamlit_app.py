c5ecc2c37343628847781c6cb11c019a28173c7a
walksuz <71907969+walksuz@users.noreply.github.com>
2022-05-12 12:00:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, spinach & rocket Smoothie')
+streamlit.text('Kale, spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
